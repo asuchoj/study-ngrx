8aa72f1c856b45f567514b603344d4a421672de4
asuchoj <asuchoj@mail.ru>
2019-01-28 23:15:53 +0300

    NGRX-3: registry store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {NgModule} from '@angular/core';
+import {StoreModule} from '@ngrx/store';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
 import {CarsFormComponent } from './cars-form/cars-form.component';
 import {CarComponent} from './car/car.component';
-
+import {carsReduer} from './redux/cars.reducer';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {CarComponent} from './car/car.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    StoreModule.forRoot({carPage: carsReduer})
   ],
   providers: [],
   bootstrap: [AppComponent]
